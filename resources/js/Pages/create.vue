9b9a1576b5a3a56f94dedb73ca2c9519e4abb6c3
giddy77 <giddysipoi@gmail>
2024-03-28 14:05:53 +0300

    intial setup

---
+++ resources/js/Pages/create.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card mt-3 col-6 offset-3">
+        <div class="card-header">
+            Featured
+        </div>
+        <div class="card-body">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Name</label>
+                    <input type="name" class="form-control" id="exampleName"
+                        placeholder="Enter name">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Phone</label>
+                    <input type="number" class="form-control" id="exampleContact"
+                        placeholder="Enter Phone">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary mt-3">Submit</button>
+            </form>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
